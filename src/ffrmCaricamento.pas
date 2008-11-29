eab0fa11bb218bd8a59a3656ec24d0f8ca8a7ed9
Andrea Martinelli <martinelli-andrea@outlook.com>
2008-11-29 13:51:56 +0000

    ffrmCaricamento

---
+++ src/ffrmCaricamento.pas
@@ -0,0 +1,29 @@
+unit ffrmCaricamento;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, fbblTinker, StdCtrls;
+
+type
+  TfrmCaricamento = class(TForm)
+    Label1: TLabel;
+    Button1: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmCaricamento: TfrmCaricamento;
+
+implementation
+
+uses ffrmTinker;
+
+{$R *.dfm}
+
+
+end.
